30ba27b1e0ad94835f4bf80efede7d93c67cb9fb
Tech-A10 <you@example.com>
2018-05-15 21:01:13 +0100

    files added

---
+++ UnitTestPractice/CSharpTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+namespace UnitTestPractice
+{
+
+	public class Calc
+	{
+		public double Add(double num1, double num2)
+		{
+			return num1 + num2;
+		}
+	}
+
+
+
+	[TestFixture()]
+    public class Examples
+    {
+		Calc CalcT = new Calc();	
+
+	  [Test()]	
+      public void Test()
+		{
+			Assert.AreEqual(CalcT.Add(1, 2), 3);
+		}
+    }
+}
---
+++ UnitTestPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
